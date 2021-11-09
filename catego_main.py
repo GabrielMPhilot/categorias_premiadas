5be0d9925657ada52728590257c246017e24195e
unknown <Gabriel Martin Philot>
2021-11-09 17:36:50 -0300

    commit5

--- catego_main.py
+++ catego_main.py
@@ -28,7 +28,7 @@ st.sidebar.image(image,caption='Eduqo - Plataforma QMágico',use_column_width=Tr
 st.sidebar.markdown('Feito por: Gabriel Philot (Tio Gibbs)')
 st.sidebar.write('#### Links associados com o projeto')
 st.sidebar.write('####')
-st.sidebar.write("##### [Github](https://github.com/GabrielMPhilot/categorias_premiadas)")
+#st.sidebar.write("##### [Github](https://github.com/GabrielMPhilot/categorias_premiadas)")
 st.sidebar.write('####')
 st.sidebar.write("##### [G.Slides](https://docs.google.com/presentation/d/1hSyXkuVwCf6QhHEPrmEeFrLOoDUxYwiqK0h_4Ck_azE/edit#slide=id.gacf8bfa375_1_201)")
 st.sidebar.write('#')
@@ -267,6 +267,7 @@ expander_cont = st.expander(" (Top 20)  -> (clique aqui 🖱️)")
 expander_cont.table(catego4m02top20)
 expander_cont = st.expander(" Toda a tabela  -> (clique aqui 🖱️)")
 expander_cont.dataframe(catego4m02)
+st.write('#### Resultado de nosso modelo (Todos namespaces):',get_table_download_link(catego4m02), unsafe_allow_html=True)
 #catego4m02
 #catego4m02top20
 """
@@ -304,3 +305,5 @@ expander_cont.dataframe(catego4m04)
 #catego4m04
 #catego4m04top20
 #'GIVE_FEEDBACK','ASK_REVISION','CREATE_TEXT_REVISION_FEEDBACK','ADD_COMMENT'
+
+
